a11214948d147446c9d3882eaf2e2456fe769375
João Luiz <ojoao.luiz07@gmail.com>
2026-04-22 21:49:26 -0300

    Tentando cadastrar o agendamento no banco de dados
    possivelmente o site vai estar dando erro

--- site/main.py
+++ site/main.py
@@ -1,3 +1,4 @@
+#Bibliotecas necessarias (se for tentar mexer no python e testar o funcionamente )
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
@@ -13,18 +14,45 @@ supabase = create_client(url,key)
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET','POST'])
-def index():
-    dados = None
+
+def comeco():   # Definindo a rota principal do site (homepage/login)
+    cliente = None
+
     if request.method == 'POST':
-        dados = {
+        cliente = {
             'client_nome': request.form.get('nome'),
             'client_numero': request.form.get('numero')
         }
-        print(dados['client_nome'])
-        supabase.table('clientes').insert(dados).execute()
-        
-    return render_template('index.html')
+
+        resposta = supabase.table('clientes').insert(cliente).execute()
+
+        print(resposta)
+        return render_template('agendamento.html', dados = cliente)
+
+    else:
+        return render_template('index.html')
+
+@app.route("/fim", methods=['GET','POST'])
+def termino(): #Definindo rota do login para o agendamento
+    agendamento = None
+
+    if request.method == 'POST':
+        data = request.form.get('data')
+        horario = request.form.get('horario')
+
+        agendamento = {
+            'servico': request.form.get('servico'),
+            'horario': f"{data} {horario}:00",
+        }
+
+        supabase.table('agendamentos').insert(agendamento).execute()
+        return render_template('fim.html')
+
+    else:
+        return render_template('index.html')
+
 
 if __name__ == '__main__':  #Faz o código rodar as rotas do Flesk
     app.run(debug=True)
