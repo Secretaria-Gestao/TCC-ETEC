f2301bec7ce7b1e3e8cbac2506bfd95b29940c98
marcelamoraesrp <marcela.moraesrp@gmail.com>
2026-05-02 14:46:47 -0300

    Add new CSS/JS and update templates

--- site/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function pegarDados() {
-    let userEmail = document.getElementsByName('email')[0].value;
-    console.log(userEmail);
-}
-
-async function mandarDados() {
-    method: 'POST'
-}
---
+++ site/static/js/validation.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+// function pegarDados() {
+//     let userEmail = document.getElementsByName('email')[0].value;
+//     console.log(userEmail);
+// }
