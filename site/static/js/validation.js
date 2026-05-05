422c60d8faa502ec37a5b990fb2236d6e09c4e5a
João Luiz <ojoao.luiz07@gmail.com>
2026-05-05 19:38:00 -0300

    Começando o BD no JS

--- site/static/js/validation.js
+++ site/static/js/validation.js
@@ -2,6 +2,9 @@ function validateEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
 }
 
+const SUPABASE_URL = "https://qtgubbbrntnltrpyywqx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_vQdySC4Ec0oLE5CRBHx-sw_DkDzLkYu"
+
 // function pegarDados() {
 //     let userEmail = document.getElementsByName('email')[0].value;
 //     console.log(userEmail);
