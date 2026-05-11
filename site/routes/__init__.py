f79d9dc29bd91bec85cbe17b0bb7e80af80187d0
JoaoLuiz07 <ojoao.luiz07@gmail.com>
2026-05-11 20:00:43 -0300

    novamente tirando oq eu fiz no trabalho

--- site/routes/__init__.py
+++ site/routes/__init__.py
@@ -1,7 +1,7 @@
-from .html_routes import html_routes
-from .json_routes import json_routes
+from .rotas_html import rotas_html
+from .rotas_json import rotas_json
 
-def register_routes(app):
-    app.register_blueprint(html_routes)
-    app.register_blueprint(json_routes)
+def register_routes(site):
+    site.register_blueprint(html_routes)
+    site.register_blueprint(json_routes)
     
