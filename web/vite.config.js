e6a47939d7a0b35d9c75dac7f9baa7dcf30bfc81
João Luiz <ojoao.luiz07@gmail.com>
2026-06-25 14:44:44 -0300

    testando sem proxy

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,22 +4,5 @@ import react from '@vitejs/plugin-react'
 // Configuracao do Vite para desenvolvimento.
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    proxy: {
-      '/api/cadastro/cliente': {
-        target: 'https://secretaria-gestao-api.vercel.app',
-        changeOrigin: true,
-      },
-      '/api/cadastro/gerente': {
-        target: 'https://secretaria-gestao-api.vercel.app',
-        changeOrigin: true,
-      },
-      '/api/cadastro/salao': {
-        target: 'https://secretaria-gestao-api.vercel.app',
-        changeOrigin: true,
-      },
-
-    },
-  },
+  
 })
