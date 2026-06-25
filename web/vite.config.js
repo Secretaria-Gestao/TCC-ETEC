4ed40a7bb80ad9f2ac9e9a6275525bdb9bd8bacb
João Luiz <ojoao.luiz07@gmail.com>
2026-06-25 15:34:52 -0300

    Requisições devem funcionar localmente também

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuracao do Vite para desenvolvimento.
 export default defineConfig({
   plugins: [react()],
-  
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
