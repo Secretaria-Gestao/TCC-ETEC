28e370819638f1ea996fb1265338d6ad2430aade
DaviRcarmo <marcela.moraesrp@gmail.com>
2026-08-28 20:41:41 -0300

    tirando erros dos erros

--- web/src/pages/Login/components/FormularioLogin/FormularioLogin.jsx
+++ web/src/pages/Login/components/FormularioLogin/FormularioLogin.jsx
@@ -14,6 +14,8 @@ function FormularioLogin() {
         senha: ""
     })
 
+    const [mandou, setMandou] = useState(0)
+
     function mudarValor(evento) {
         const nomeCampo = evento.target.name
         const valorCampo = evento.target.value
@@ -27,6 +29,7 @@ function FormularioLogin() {
     async function enviarFormulario(event) {
         event.preventDefault()
 
+        setMandou(1)
         const resultado = await logar(formulario.email, formulario.senha)
 
         if (resultado) {
@@ -48,17 +51,27 @@ function FormularioLogin() {
                         <label htmlFor="email">Email</label>
                         <input type="email" name="email" id="email" placeholder="seu@email.com" onChange={mudarValor} />
 
-                        {formulario.email ? (
-                            <div className="error" id="email-required-error">
-                                Email é obrigatório
-                            </div>
-                        ): <div></div>}
+                        {
+                            mandou > 0 && (
+                                !formulario.email ? (
+                                    <div className="error" id="password-required-error">
+                                        Email é obrigatório
+                                    </div>
+                                )
+                                : <div></div>
+                            )
+                        }
 
-                        {!/\S+@\S+\.\S+/.test(formulario.email) && (
-                            <div className="error" id="email-invalid-error">
-                                Email é inválido
-                            </div>
-                        )}	
+                        {
+                            formulario.email.length > 0 && (
+                                
+                                !/\S+@\S+\.\S+/.test(formulario.email) && (
+                                    <div className="error" id="email-invalid-error">
+                                    Email é inválido
+                                </div>
+                                )
+                            )
+                        }	
                         
                     </div>
 
@@ -66,9 +79,17 @@ function FormularioLogin() {
                         <label htmlFor="senha">Senha</label>
                         <input type="password" name="senha" id="password" placeholder="sua senha" onChange={mudarValor} />
 
-                        <div className="error" id="password-required-error">
-                            Senha é obrigatória
-                        </div>
+                        {
+                            mandou > 0 && (
+                                !formulario.senha ? (
+                                    <div className="error" id="password-required-error">
+                                        Senha é obrigatória
+                                    </div>
+                                )
+                                : <div></div>
+                            )
+                        }
+                        
                     </div>
 
                     <button type="button" className="clear" id="recover-password-button">
