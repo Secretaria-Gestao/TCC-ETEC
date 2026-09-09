18966cbe1cd5b9831af2cc2da8c1ae575682e2d1
João Luiz <ojoao.luiz07@gmail.com>
2026-09-08 22:25:08 -0300

    Atualizando a barra lateral

--- web/src/components/BarraLateral/BarraLateral.jsx
+++ web/src/components/BarraLateral/BarraLateral.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router'
 
 import './BarraLateral.css'
+import { menuAdmin } from './listaMenus'
 
 function BarraLateral() {
     return (
@@ -28,66 +29,19 @@ function BarraLateral() {
                 <p className='text-[14px] w-max'> Página principal</p>
             </NavLink>
 
-            <NavLink
-                to="/cadastro/colaborador"
-                className={({ isActive }) => `flex gap-3 p-2 rounded-md my-1 transition-colors ${isActive ? 'bg-[#2f3d50]' : 'hover:bg-[#2f3d50]'}`}
-            >
-
-                <svg
-                    className="w-5 h-5 shrink-0"
-                    viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" strokeWidth="1.75"
-                    strokeLinecap="round" strokeLinejoin="round"
+            {menuAdmin.map(linha => (
+                <NavLink
+                    to={linha.to}
+                    className={({ isActive }) => `flex gap-3 p-2 rounded-md my-1 transition-colors ${isActive ? 'bg-[#2f3d50]' : 'hover:bg-[#2f3d50]'}`}
                 >
 
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                   {linha.icone()}
 
-                </svg>
+                    <p className='text-[14px] w-max'> {linha.titulo} </p>
+                </NavLink>
+            ))}
 
-                <p className='text-[14px] w-max'> Cadastro de profissionais</p>
-            </NavLink>
-
-            <NavLink
-                to="/admin/gerenciador-servicos"
-                className={({ isActive }) => `flex gap-3 items-center p-2 rounded-md my-1 transition-colors ${isActive ? 'bg-[#2f3d50]' : 'hover:bg-[#2f3d50]'}`}
-            >
-
-                <svg className="w-5 h-5 shrink-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round">
-                    <circle cx="6" cy="6" r="3" />
-                    <circle cx="6" cy="18" r="3" />
-                    <line x1="20" y1="4" x2="8.12" y2="15.88" />
-                    <line x1="14.47" y1="14.48" x2="20" y2="20" />
-                    <line x1="8.12" y1="8.12" x2="12" y2="12" />
-                </svg>
-
-                <p className="text-[14px]">Serviços fornecidos</p>
-
-            </NavLink>
-
-            <NavLink
-                to="/admin/gerenciador-profissionais"
-                className={({ isActive }) => `flex gap-3 items-center p-2 rounded-md my-1 transition-colors ${isActive ? 'bg-[#2f3d50]' : 'hover:bg-[#2f3d50]'}`}
-            >
-                <svg
-                    className="w-5 h-5 shrink-0"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.75"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
-
-                <p className="text-[14px]">Profissionais cadastrados</p>
-            </NavLink>
+           
         </aside>
     )
 }
---
+++ web/src/components/BarraLateral/listaMenus.jsx
@@ -0,0 +1,53 @@
+export const menuAdmin = [
+    {
+        "to": "/cadastro/colaborador",
+        "titulo": "Cadastro de profissionais",
+        "icone": () => (
+            <svg
+            className="w-5 h-5 shrink-0"
+            viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" strokeWidth="1.75"
+            strokeLinecap="round" strokeLinejoin="round"
+        >
+            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+            <circle cx="9" cy="7" r="4" />
+            <line x1="19" y1="8" x2="19" y2="14" />
+            <line x1="16" y1="11" x2="22" y2="11" />
+        </svg>
+        )
+    },
+    {
+        "to": "/admin/gerenciador-servicos",
+        "titulo": "Serviços fornecidos",
+        "icone": () => (
+            <svg className="w-5 h-5 shrink-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round">
+                <circle cx="6" cy="6" r="3" />
+                <circle cx="6" cy="18" r="3" />
+                <line x1="20" y1="4" x2="8.12" y2="15.88" />
+                <line x1="14.47" y1="14.48" x2="20" y2="20" />
+                <line x1="8.12" y1="8.12" x2="12" y2="12" />
+            </svg>
+
+        )
+    },
+    {
+        "to": "/admin/gerenciador-profissionais",
+        "titulo": "Profissionais cadastrados",
+        "icone": () => (
+            <svg
+                className="w-5 h-5 shrink-0"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.75"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+        )
+    },
+]
